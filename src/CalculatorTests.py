75a25840cb6a9385652bb2ab5a47fcacab3800b9
Edward Kong <dejingk@gmail.com>
2021-07-05 20:05:47 -0400

    Feature: add result property test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,12 +1,19 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
